9c2f4b4c4ae05fa0f596248e01825c358fc2fbd5
Brian <bsquintero_usistemas@intep.edu.co>
2025-05-13 09:06:07 -0500

    feat(theme): add theme provider and update global styles
    Introduce a new ThemeProvider component to handle theme management using next-themes. Update global CSS variables and styles to support light and dark themes, ensuring consistent theming across the application.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,8 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css'; // Asegúrate que Tailwind está configurado aquí
 import { AuthProvider } from './context/AuthContext'; // Importa el AuthProvider
-import { copernicusFont } from './fonts'
+import { copernicusFont } from './fonts';
+import { ThemeProvider } from "../components/theme-provider";
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -40,11 +41,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>): React.ReactElement {
   return (
-    <html lang="es">
+    <html lang="es" suppressHydrationWarning>
       <body className={`${inter.className} ${copernicusFont.variable}`}>
-        {/* Envuelve la aplicación con AuthProvider */}
-        <AuthProvider>{children}</AuthProvider>
-        {/* Es muy importante el componente AuthProvider ya que se encarga de la logica que mantiene la sesion el usuario */}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {/* Envuelve la aplicación con AuthProvider */}
+          <AuthProvider>{children}</AuthProvider>
+          {/* Es muy importante el componente AuthProvider ya que se encarga de la logica que mantiene la sesion el usuario */}
+        </ThemeProvider>
       </body>
     </html>
   );
