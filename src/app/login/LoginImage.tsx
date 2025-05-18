2d8c34e5488e36062a90eec7690aa8cb713a8d07
Brian <bsquintero_usistemas@intep.edu.co>
2025-05-18 17:40:32 -0500

    refactor(login): remove unused loginPrueba page and update LoginImage path
    The loginPrueba page was deleted as it was redundant and no longer needed. Additionally, the image path in LoginImage.tsx was updated to use a direct path instead of an imported image for better consistency and maintainability.

--- src/app/login/LoginImage.tsx
+++ src/app/login/LoginImage.tsx
@@ -1,11 +1,10 @@
 import Image from 'next/image';
-import PhotoLogin from '../../../img/photologin.png';
 
 export const LoginImage = () => {
     return (
         <div className="hidden lg:block bg-[#f0e9de] rounded-2xl h-50 relative lg:h-auto overflow-hidden">
             <Image 
-                src={PhotoLogin}
+                src="/img/photologin.png"
                 alt="Login" 
                 width={1200}
                 height={1200}
