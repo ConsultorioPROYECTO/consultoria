c200a366babf0cf1aa990b7798f47e66bf96a4ed
SantiagoPrada2005 <santiagopradamoreno@gmail.com>
2025-07-13 15:39:11 -0500

    refactor(AddStaffForm): remove unused X icon import

--- src/app/dashboard/rol/Admin/_compo/AddStaffForm.tsx
+++ src/app/dashboard/rol/Admin/_compo/AddStaffForm.tsx
@@ -4,7 +4,7 @@ import { useState } from 'react';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
-import { UserPlus, ChevronsUpDown, Check, X } from 'lucide-react';
+import { UserPlus, ChevronsUpDown, Check } from 'lucide-react';
 import { toast } from 'sonner';
 import { useMedicalServices } from '@/hooks/useMedicalServices';
 import {
