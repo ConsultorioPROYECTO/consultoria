547fb0a9070720acf8261b19ce0fc56e2a664e57
SantiagoPrada2005 <santiagopradamoreno@gmail.com>
2025-07-13 14:56:05 -0500

    refactor(AssistantDashboard): improve layout structure and scrolling behavior
    Restructure the layout to use semantic HTML5 main tag and add overflow handling
    Adjust padding and spacing for better responsiveness across screen sizes

--- src/app/dashboard/rol/Assistant/page.tsx
+++ src/app/dashboard/rol/Assistant/page.tsx
@@ -6,13 +6,15 @@ import { CreatePatientModal } from "./_compo/CreatePatientModal";
 export default function AssistantDashboard() {
 
   return (
-        <div className="flex flex-1 flex-col">
-          <div className="@container/main flex flex-1 flex-col gap-2">
-            <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
-              <CreateAppointmentModal/>
-              <CreatePatientModal />
+        <div className="flex flex-1 flex-col overflow-y-auto">
+          <main className="flex-1 space-y-6 pb-4 md:pb-4 lg:pb-6 px-4 md:px-4 lg:px-6 pt-2 md:pt-2 lg:pt-2">
+            <div  className="flex flex-col @lg:flex-row @lg:items-center @lg:justify-between mb-6">
+              <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+                <CreateAppointmentModal/>
+                <CreatePatientModal />
+              </div>
             </div>
-          </div>
+          </main>
         </div>
   )
 }
