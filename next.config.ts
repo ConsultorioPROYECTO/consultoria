9e65b240ab2776d8a3afafd8ee0e168efacc1bd3
rubiuscoffee <bsquintero_usistemas@intep.edu.co>
2025-07-19 17:54:05 -0500

    fix: remove commented-out headers config in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -20,7 +20,8 @@ const nextConfig: NextConfig = {
  * It includes headers for DNS prefetch control, HSTS, X-Frame-Options,
  * X-Content-Type-Options, X-XSS-Protection, Referrer-Policy, Permissions-Policy,
  * and Content-Security-Policy.
- */
+ 
+
   headers: async () => {
     return [
       {
@@ -75,7 +76,7 @@ const nextConfig: NextConfig = {
         ],
       },
     ];
-  },
+  },*/
 };
 
 export default nextConfig;
