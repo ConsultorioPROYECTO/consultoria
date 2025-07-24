f8887753459022592a925d0011bea8f270ec7c68
rubiuscoffee <bsquintero_usistemas@intep.edu.co>
2025-07-23 19:50:57 -0500

    style(login): fix full-width text alignment in mobile view

--- src/app/login/login-form/LoginContent.tsx
+++ src/app/login/login-form/LoginContent.tsx
@@ -29,7 +29,7 @@ export function LoginContent() {
   return (
     <div className="flex flex-col min-h-screen w-full items-center justify-end lg:justify-center px-4 py-8">
       <div className='inset-0 flex items-center justify-center lg:hidden pointer-events-none'>
-        <span className="text-[10rem] font-extrabold text-foreground opacity-8 select-none">Irina</span>
+        <span className="text-[10rem] font-extrabold text-foreground opacity-8 w-full select-none">Irina</span>
       </div>
       <div className="flex flex-col gap-4 justify-center max-w-sm">
         
