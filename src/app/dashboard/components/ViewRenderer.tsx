fc661757ccc0e0128da279ef92c4a55faa0615d1
rubiuscoffee <bsquintero_usistemas@intep.edu.co>
2025-07-26 14:49:32 -0500

    style(dashboard): adjust bottom padding for mobile views in wrapper components

--- src/app/dashboard/components/ViewRenderer.tsx
+++ src/app/dashboard/components/ViewRenderer.tsx
@@ -32,7 +32,7 @@ const ConfigurationView = dynamic(() => import('../configurations/config-view'),
 
 const CalendarWrapper = () => (
   <div className="flex flex-1 flex-col overflow-hidden">
-    <main className="flex-1 space-y-6 pb-4 md:pb-4 lg:pb-6 px-4 md:px-4 lg:px-6 pt-2 md:pt-2 lg:pt-2">
+    <main className="flex-1 space-y-6 pb-22 md:pb-4 lg:pb-6 px-4 md:px-4 lg:px-6 pt-2 md:pt-2 lg:pt-2">
       <div className="h-full w-full flex flex-col">
         <Suspense fallback={<LoadingSpinner />}>
           <CalendarView />
@@ -44,7 +44,7 @@ const CalendarWrapper = () => (
 
 const OrganizationConfigWrapper = () => (
   <div className="flex flex-1 flex-col overflow-hidden">
-    <main className="flex-1 space-y-6 pb-4 md:pb-4 lg:pb-6 px-4 md:px-4 lg:px-6 pt-2 md:pt-2 lg:pt-2">
+    <main className="flex-1 space-y-6 pb-22 md:pb-4 lg:pb-6 px-4 md:px-4 lg:px-6 pt-2 md:pt-2 lg:pt-2">
       <div className="h-full w-full flex flex-col">
         <Suspense fallback={<LoadingSpinner />}>
           <OrganizationConfigView />
@@ -56,7 +56,7 @@ const OrganizationConfigWrapper = () => (
 
 const ConfigurationWrapper = () => (
   <div className="flex flex-1 flex-col overflow-hidden">
-    <main className="flex-1 space-y-6 pb-4 md:pb-4 lg:pb-6 px-4 md:px-4 lg:px-6 pt-2 md:pt-2 lg:pt-2">
+    <main className="flex-1 space-y-6 pb-22 md:pb-4 lg:pb-6 px-4 md:px-4 lg:px-6 pt-2 md:pt-2 lg:pt-2">
       <div className="h-full w-full flex flex-col">
         <Suspense fallback={<LoadingSpinner />}>
           <ConfigurationView />
